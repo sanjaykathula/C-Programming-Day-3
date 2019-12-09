665930600433a7b129b158b8aa6d879011886678
Sanjay Kathula <58501735+sanjaykathula@users.noreply.github.com>
2019-12-09 12:06:47 +0530

    Add files via upload

---
+++ Untitled16.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+main()
+{
+	int i,j,k,n,count;
+	printf("\n enter n value :");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		count=0;
+		for(j=i,k=1;k<=j;k++)
+		{
+			if(j%k==0)
+			count++;
+		}
+		if(count==2)
+		printf("\n%d",j);
+	}
+}
